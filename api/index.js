0ad7b97d5410cf9184c0cd071a8d824e5c531a70
lennart1s <sandbothe.lennart@gmail.com>
2023-04-13 15:25:44 +0200

    API Express init

--- api/index.js
+++ api/index.js
@@ -1,1 +1,13 @@
-console.log('420flowScope bitches!');
+require('dotenv').config({ path: '../.env.local' });
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const port = process.env.API_PORT || 7071;
+console.log('Listening on port', port);
+app.listen(port);
