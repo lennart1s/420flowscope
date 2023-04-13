7d9d95e3e44fe5d552e81eebb73e093ce1dedc82
lennart1s <sandbothe.lennart@gmail.com>
2023-04-13 19:59:39 +0200

    Connected login

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,12 +5,39 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    profile: null,
   },
   getters: {
+    profile: (state) => state.profile,
   },
   mutations: {
   },
   actions: {
+    async login({ state }, { email, password }) {
+      try {
+        const resp = await fetch('http://localhost:7071/auth/login', {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: new URLSearchParams({
+            email,
+            password,
+          }),
+        });
+        if (resp.status !== 200) {
+          state.profile = null;
+          return null;
+        }
+        const profile = await resp.json();
+
+        state.profile = profile;
+        return profile;
+      } catch (e) {
+        state.profile = null;
+        return null;
+      }
+    },
   },
   modules: {
   },
