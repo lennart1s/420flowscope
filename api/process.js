d628a9c518e59b1742ab9a6bebdac60754f40632
lennart1s <sandbothe.lennart@gmail.com>
2023-04-13 16:33:19 +0200

    Added get processTypes endpoint

--- api/process.js
+++ api/process.js
@@ -2,6 +2,23 @@ const router = require('express').Router();
 const { eguard, assert } = require('./eguard');
 
 const processes = require('../db/processes.json');
+const processTypes = require('../db/processTypes.json');
+
+router.get('/types/:typeId?', eguard(async (req, res) => {
+  const { typeId } = req.params;
+
+  if (typeId) {
+    assert(processTypes[typeId], 404, `no type found with id '${typeId}'`);
+
+    const type = JSON.parse(JSON.stringify(processTypes[typeId]));
+    type.type = typeId
+
+    res.json(type).send();
+    return;
+  }
+
+  res.json(processTypes);
+}));
 
 router.get('/:id', eguard(async (req, res) => {
   const { id } = req.params;
