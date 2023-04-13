575eaa91112c8025ce0109adeef3f4296fc7e046
lennart1s <sandbothe.lennart@gmail.com>
2023-04-13 14:58:56 +0200

    Frontend init commit

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default {
+  name: 'HomeView',
+
+  components: {
+    HelloWorld,
+  },
+};
+</script>
