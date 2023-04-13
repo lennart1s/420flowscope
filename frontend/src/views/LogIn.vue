d7037e012b37296fe83007384167219eaea99fbd
DanielH191 <daniel.huesmann@tu-dortmund.de>
2023-04-13 17:56:04 +0200

    login view added

---
+++ frontend/src/views/LogIn.vue
@@ -0,0 +1,43 @@
+<template>
+  <div style="width: 40%; left: 30%; border: 2px grey solid; border-radius: 15px;">
+     <v-form v-model="valid">
+      <v-container>
+        <v-col>
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="password"
+            label="Password"
+            required
+            type="password"
+          ></v-text-field>
+        </v-col>
+      </v-container>
+
+    </v-form>
+      <v-container md="3" style="display: flex; justify-content: center; margin-bottom: ">
+        <v-btn
+        @click="test()">
+          Log in
+        </v-btn>
+      </v-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LogIn',
+  methods: {
+    test() {
+      console.log('hdasd');
+    },
+  },
+};
+</script>
