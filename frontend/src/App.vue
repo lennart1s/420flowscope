3c544bc639368f2b89caa74ab2cf519a5207e473
DanielH191 <daniel.huesmann@tu-dortmund.de>
2023-04-14 12:43:39 +0200

    revert

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -48,80 +48,21 @@
     <v-main color="rgb(255, 255, 255, 0.88)">
       <router-view/>
     </v-main>
-
-  <ApplicationForm name="Verbraucherschlichtungsprozess"
-    :data="array" style="display: flex; justify-content: center"/>
+  <AddProcess></AddProcess>
   </v-app>
 </template>
 
 <script>
+import AddProcess from './views/AddProcess.vue';
+
 export default {
   name: 'App',
-
   data: () => ({
-    array: [
-      {
-        type: 'TextArea',
-        name: 'kurze Beschreibung des Vorfalls',
-        required: true,
-        dependentOn: '',
-      },
-      {
-        type: 'date',
-        name: 'Datum des Vorfalls',
-        required: false,
-        dependentOn: '',
-      },
-      {
-        type: 'checkbox',
-        name: 'Antrag für jemand anderen',
-        required: true,
-        dependentOn: '',
-      },
-      {
-        type: 'Headline',
-        name: 'Adresse geschädigter',
-        required: false,
-        dependentOn: 'Antrag für jemand anderen',
-      },
-      {
-        type: 'String',
-        name: 'Vorname',
-        required: true,
-        dependentOn: 'Antrag für jemand anderen',
-      },
-      {
-        type: 'String',
-        name: 'Nachname',
-        required: true,
-        dependentOn: 'Antrag für jemand anderen',
-      },
-      {
-        type: 'String',
-        name: 'Straße und Hausnummer',
-        required: true,
-        dependentOn: 'Antrag für jemand anderen',
-      },
-      {
-        type: 'number',
-        name: 'Postleitzahl',
-        required: true,
-        dependentOn: 'Antrag für jemand anderen',
-      },
-      {
-        type: 'String',
-        name: 'Firma',
-        required: true,
-        dependentOn: '',
-      },
-      {
-        type: 'file-',
-        name: 'Laden Sie Dokumente hoch, die zum Verständnis des Falls beitragen',
-        required: true,
-        dependentOn: '',
-      },
-    ],
+    // TODO
   }),
+  components: {
+    AddProcess,
+  },
 };
 </script>
 
