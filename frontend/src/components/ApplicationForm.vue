b8b281a9389512258f823f06957210bfa0d9fa17
DanielH191 <daniel.huesmann@tu-dortmund.de>
2023-04-14 10:10:13 +0200

    ..

--- frontend/src/components/ApplicationForm.vue
+++ frontend/src/components/ApplicationForm.vue
@@ -1,48 +1,51 @@
 <template>
-  <v-container>
+  <v-container class="flexCon">
     <p>{{ name }}</p>
-    <v-col
-     :key="entry.name"
-     v-for="entry in data"
-     style="display: flex; flex-direction: column">
+
+    <v-container
+
+     :key="`${entry.name}-${idx}`"
+     v-for="entry, idx in data">
         <v-text-field
           :class="'posDep ^'+entry.dependentOn+'_ '"
           v-if="entry.type === 'String'"
           :label="entry.name"
           :required="entry.required"
-          v-model="text"
+          :style="{'display: ': false ? 'none' : 'block'}"
         >
-      </v-text-field>
-        <v-file-input
-          :class="'posDep ^'+entry.dependentOn+'_ '"
-          v-if="entry.type.indexOf('file-') > -1"
-          :accept="entry.type.replace('file-','')+'/*'"
-          :label="entry.name"
-        ></v-file-input>
-      <div>
-        <v-text-field
-          :class="'posDep ^'+entry.dependentOn+'_ '"
-          v-if="entry.type==='number'"
-          :label="entry.name"
-          :required="entry.required"
-          v-model="text"
-        ></v-text-field>
-      </div>
-      <div>
-      <v-row :class="'posDep ^'+entry.dependentOn+'_'" v-if="entry.type === 'date'">
+        </v-text-field>
+
+      <v-file-input
+        :class="'posDep ^'+entry.dependentOn+'_ '"
+        v-if="entry.type.indexOf('file-') > -1"
+        :accept="entry.type.replace('file-','')+'/*'"
+        :label="entry.name"
+      ></v-file-input>
+
+      <v-text-field
+        :class="'posDep ^'+entry.dependentOn+'_ '"
+        v-if="entry.type === 'number'"
+        :label="entry.name"
+        :required="entry.required"
+      ></v-text-field>
+
+      <v-col style="text-align: center" :class="'posDep ^'+entry.dependentOn+'_'"
+      v-if="entry.type === 'Headline'">
+        <p style="text-align: center">{{ entry.name }}</p>
+      </v-col>
+      <v-col :class="'posDep ^'+entry.dependentOn+'_'" v-if="entry.type === 'date'"
+      :style="{'display': entry.dependentOn.length>1 ? 'none' : 'block'}">
         <p>{{ entry.name }}</p>
         <v-date-picker :label="entry.name"></v-date-picker>
-      </v-row>
-      <div >
-        <v-checkbox
-          v-if="entry.type === 'checkbox'"
-          :label="entry.name"
-          :id="entry.name"
-          @click="checkDeps()"
-        ></v-checkbox>
-      </div>
-    </div>
-    </v-col>
+      </v-col>
+
+      <v-checkbox
+        v-if="entry.type === 'checkbox'"
+        :label="entry.name"
+        :id="entry.name"
+        @click="checkDeps()"
+      ></v-checkbox>
+    </v-container>
   </v-container>
 </template>
 
@@ -61,36 +64,42 @@ export default {
   },
   methods: {
     isChecked(dependentOn) {
-      console.log('dependentOn: ', dependentOn);
       if (dependentOn === null || dependentOn === '') {
         return true;
       }
       const box = document.getElementById(dependentOn);
-      console.log('box: ', box.ariaChecked);
       if (box === null || box.ariaChecked === null) {
         return true;
       }
       return box.ariaChecked;
     },
     checkDeps() {
+      console.log('checkdeps');
       const array = document.getElementsByClassName('posDep');
       for (let i = 0; i < array.length; i += 1) {
-        console.log('------------------');
-        const e = array[i].className;
-        const b = this.isChecked(e.substring(e.indexOf('^') + 1, e.indexOf('_')));
-        if (b === true) {
-          console.log('checked', b);
-          if (e != null && e !== undefined && e.style != null && e.style !== undefined) {
-            e.style.visibility = 'visible';
+        console.log(i);
+        const e = array[i];
+        const c = e.className;
+        const b = this.isChecked(c.substring(c.indexOf('^') + 1, c.indexOf('_')));
+        if (b !== 'false' && b) {
+          if (e != null && e !== undefined) {
+            e.style.display = 'none';
           }
-        } else {
-          console.log('not checked', b);
-          if (e != null && e !== undefined && e.style != null && e.style !== undefined) {
-            e.style.visibility = 'visible';
-          }
+        } else if (e != null && e !== undefined) {
+          e.style.display = 'block';
         }
       }
     },
   },
+  mounted() {
+    console.log('data', this.data);
+    this.checkDeps();
+  },
 };
+
 </script>
+<style scoped>
+.flexCon{
+  display: flex; flex-direction: column
+}
+</style>
