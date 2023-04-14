6aeeb5ffa1e13a8b8d683af646532fe760b84468
Anton Hackl <antonhackl0@gmail.com>
2023-04-14 12:11:50 +0200

    Revert "Remove unused code"
    This reverts commit 85b9f28b6446ba2970514a56f89b3d9516d0c118.

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -21,6 +21,15 @@ export default {
   components: {
     CardList,
   },
+  computed: {
+    ...mapGetters(['profile']),
+  },
+  // created: () => {
+  //   console.log(mapGetters(['profile']));
+  //   if (this.profile === undefined) {
+  //     this.$router.push('/login');
+  //   }
+  // },
 };
 
 </script>
